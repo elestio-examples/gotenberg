2e81bc2378d39bf44b9b1fe27bee20c3124aeda1
Prem chavhan <pchavhan@logiquad.com>
2023-10-23 20:15:59 +0530

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 cp ./build/Dockerfile ./
+sed -i "s~ARG GOLANG_VERSION~ARG GOLANG_VERSION=1.21~g" Dockerfile
+sed -i "s~ARG GOTENBERG_VERSION~ARG GOTENBERG_VERSION=snapshot~g" Dockerfile
+sed -i "s~ARG GOTENBERG_USER_GID~ARG GOTENBERG_USER_GID=1001~g" Dockerfile
+sed -i "s~ARG GOTENBERG_USER_UID~ARG GOTENBERG_USER_UID=1001~g" Dockerfile
+sed -i "s~ARG PDFTK_VERSION~ARG PDFTK_VERSION=v3.3.3~g" Dockerfile
+sed -i "s~ARG NOTO_COLOR_EMOJI_VERSION~ARG NOTO_COLOR_EMOJI_VERSION=v2.038~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/gotenberg:latest | docker load
